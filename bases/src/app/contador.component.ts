7be85ecc9ec97bcb43e0eeab702a9af3304b6a62
Enrique <octepc.97@gmail.com>
2022-12-28 17:10:04 -0500

    worked with the directories

---
+++ bases/src/app/contador.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-contador',
+  template: `
+    <h1>{{ title }}</h1>
+
+    <h3>
+      La base es: <strong>{{ base }}</strong>
+    </h3>
+
+    <button class="btn btn-primary" (click)="acumulate(1)">+{{ base }}</button>
+    <span>{{ number }}</span>
+    <button class="btn btn-danger" (click)="acumulate(-1)">-{{ base }}</button>
+  `
+})
+
+export class ContadorComponent {
+
+  title: string= 'Contador App';
+  number: number = 0;
+
+  base: number = 293884;
+
+  acumulate(value:number){
+    this.number += this.base * value
+  }
+
+
+}
