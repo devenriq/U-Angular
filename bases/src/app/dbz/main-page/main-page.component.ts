b071bcb8cd5673262da980a2b44ee2f8930d9428
Enrique <octepc.97@gmail.com>
2022-12-29 18:13:56 -0500

    worked with two way data biding using ngModel and the special notation [(ngModel)] to refer a variable which is changing

--- bases/src/app/dbz/main-page/main-page.component.ts
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+
+  nuevo: Personaje={
+    nombre:'Truncks',
+    poder: 13480
+  }
+
   agregar(){
-    console.log('hey')
+    console.log(this.nuevo)
+  }
+
+  changeName(event:any){
+    console.log(event.target.value)
   }
 }
